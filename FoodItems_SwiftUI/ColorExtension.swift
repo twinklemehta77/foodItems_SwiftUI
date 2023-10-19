d9663f4b7177f9412e54bebef3ff827b550da15c
twinklemehta77 <er.twinklemehta@gmail.com>
2023-10-18 22:44:58 -0400

    Color Extension

---
+++ FoodItems_SwiftUI/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  FoodItems_SwiftUI
+//
+//  Created by Twinkle Mehta on 18/10/23.
+//
+
+import Foundation
---
+++ FoodItems_SwiftUI/View/CustomSearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSearchBarView.swift
+//  FoodItems_SwiftUI
+//
+//  Created by Twinkle Mehta on 18/10/23.
+//
+
+import SwiftUI
+
+struct CustomSearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSearchBarView()
+}
