9eaa2c39c1d52c5eb228686f05d38965de8418a9
twinklemehta77 <er.twinklemehta@gmail.com>
2023-10-18 20:47:04 -0400

    Initial commit

---
+++ FoodItems_SwiftUI/FoodItems_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodItems_SwiftUIApp.swift
+//  FoodItems_SwiftUI
+//
+//  Created by Twinkle Mehta on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodItems_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
